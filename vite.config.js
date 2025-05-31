ecd7a167774578afc030d614e24b4abf8c580fda
Maksym Bazchenko <mbazchenko@gmail.com>
2025-05-31 17:08:47 +0300

    add image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,40 @@
+// import { defineConfig } from 'vite';
+
+// import vituum from 'vituum';
+// import nunjucks from '@vituum/vite-plugin-nunjucks';
+// import postcss from '@vituum/vite-plugin-postcss';
+
+// export default defineConfig({
+//     build: {
+//         assetsInlineLimit: 0,
+//     },
+//     plugins: [
+//         vituum(),
+//         nunjucks(),
+//         postcss(),
+//     ],
+    
+// })
+
 import { defineConfig } from 'vite';
 
 import vituum from 'vituum';
 import nunjucks from '@vituum/vite-plugin-nunjucks';
-import postcss from '@vituum/vite-plugin-postcss'
+import postcss from '@vituum/vite-plugin-postcss';
+import imageOptimizer from '@bro-academy/vite-plugin-image-optimizer';
+import svgSprite from '@bro-academy/vite-plugin-svg-sprite';
 
 export default defineConfig({
-    build: {
-        assetsInlineLimit: 0,
-    },
-    plugins: [
-        vituum(),
-        nunjucks(),
-        postcss(),
-    ],
-})
+  build: {
+    assetsInlineLimit: 0,
+  },
+  plugins: [
+    vituum(),
+    nunjucks(),
+    postcss(),
+    imageOptimizer({
+      dimensions: true,
+    }),
+    svgSprite(),
+  ],
+});
